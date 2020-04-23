252faa55bee7fc135a9390b2b85c557a39caa635
Aaron Meriwether <me@ameriwether.com>
2020-04-23 11:54:52 -0500

    Remove all non-core dependencies

--- src/main/java/properties2json/App.java
+++ src/main/java/properties2json/App.java
@@ -1,15 +1,29 @@
 package properties2json;
 
 import java.io.IOException;
+import java.util.Map.Entry;
 import java.util.Properties;
-
-import org.json.JSONObject;
-
 
 public class App {
     public static void main(String[] args) throws IOException {
         Properties prop = new Properties();
         prop.load(System.in);
-        System.out.println(new JSONObject(prop).toString());
+        // System.out.println(new JSONObject(prop).toString());
+        System.out.append('{');
+        boolean needsComma = false;
+        for(Entry<Object, Object> entry : prop.entrySet()) {
+            if(needsComma) {
+                System.out.append(',');
+            }
+            System.out.append(
+                "\"" +
+                entry.getKey().toString().replace("\\", "\\\\").replace("\"", "\\\"") +
+                "\":\"" +
+                entry.getValue().toString().replace("\\", "\\\\").replace("\"", "\\\"") +
+                "\""
+            );
+            needsComma = true;
+        }
+        System.out.append('}');
     }
 }
